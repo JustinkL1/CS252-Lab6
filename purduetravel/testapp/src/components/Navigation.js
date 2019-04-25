76ff69e249c7a1993635a280b1e7ea11351ae117
Justin <lam46@purdue.edu>
2019-04-25 02:17:09 -0400

    merge

---
+++ purduetravel/testapp/src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import SignOutButton from './SignOut';
+import * as routes from '../constants/routes';
+
+import Button from '@material-ui/core/Button';
+
+
+const Navigation = ({ authUser }) =>
+  <div>
+    { authUser
+        ? <NavigationAuth />
+        : <NavigationNonAuth />
+    }
+  </div>
+
+const NavigationAuth = () =>
+ 
+  <ul>
+    <Button color="primary" component={Link} to = {routes.HOME}>
+      HOME
+    </Button>
+
+    <Button>
+      <Link to="/Forum_create" >
+        Add
+    </Link>
+    </Button>
+    
+    
+    <SignOutButton/>
+  </ul>
+
+const NavigationNonAuth = () =>
+  <ul>
+    <Button component={Link} to = {routes.LANDING}>
+      Sign In
+    </Button>  
+  </ul>
+
+  const mapStateToProps = (state) => ({
+    authUser: state.sessionState.authUser,
+});
+
+export default connect(mapStateToProps)(Navigation);
