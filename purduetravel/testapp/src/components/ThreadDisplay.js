fb766315f40651d4f687009ea0b79b46a075866f
Justin <lam46@purdue.edu>
2019-04-25 04:20:24 -0400

    thread display

---
+++ purduetravel/testapp/src/components/ThreadDisplay.js
@@ -0,0 +1,209 @@
+import React, { Component } from 'react';
+import {firebase} from '../../shared/firebase'
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import TableHead from '@material-ui/core/TableHead';
+
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+// import {Table, TableBody, TableCell, TableFooter, TablePagination, TableRow, Paper, IconButton, TableHead, withStyles} from '@material-ui/core'
+// import {KeyboardArrowLeft, KeyboardArrowRight} from '@material-ui/icons';
+
+
+const actionsStyles = theme => ({
+  root: {
+    flexShrink: 0,
+    color: theme.palette.text.secondary,
+    marginLeft: theme.spacing.unit * 2.5,
+  },
+});
+
+class TablePaginationActions extends React.Component {
+  handleBackButtonClick = event => {
+    this.props.onChangePage(event, this.props.page - 1);
+  };
+
+  handleNextButtonClick = event => {
+    this.props.onChangePage(event, this.props.page + 1);
+  };
+
+  render() {
+    const { classes, count, page, rowsPerPage, theme } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <IconButton
+          onClick={this.handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="Previous Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={this.handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="Next Page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+      </div>
+    );
+  }
+}
+
+TablePaginationActions.propTypes = {
+  classes: PropTypes.object.isRequired,
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(
+  TablePaginationActions,
+);
+
+let counter = 0;
+function createData(name, calories, fat) {
+  counter += 1;
+  return { id: counter, name, calories, fat };
+}
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    
+  },
+  table: {
+    minWidth: 500,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+});
+
+class ThreadDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = firebase.firestore().collection("forum").orderBy("mtime", "desc");
+    this.unsubscribe = null;
+    this.state = {
+      rows: [],
+      page: 0,
+      rowsPerPage: 5,
+    };
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const rows = [];
+    querySnapshot.forEach((doc) => {
+      const { title, description, mtime, image, username } = doc.data();
+      rows.push({
+        key: doc.id,
+        doc, // DocumentSnapshot
+        title,
+        description,
+        mtime,
+        image,
+        username,
+      });
+    });
+    this.setState({
+      rows
+   });
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { rows, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    var dafault_url = "https://via.placeholder.com/150";
+
+    return (
+      <Paper className={classes.root}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell> </TableCell>
+                <TableCell>Title</TableCell>
+                <TableCell>Author</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Date</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+                return (
+                  <TableRow key={row.key}>
+                    <TableCell>
+                      {row.image !== " "?<img className="Item_image" src={row.image} alt="Item" />:<img className="Item_image"  alt="Item" />}
+                      {/* <img className="Item_image" src={row.image?row.image:logo} alt="Item" /> */}
+                    </TableCell>
+                    <TableCell>
+                      <Link to={`/Forum_show/${row.key}`}>{row.title}</Link>
+                    </TableCell>
+                    <TableCell>{row.username}</TableCell>
+                    <TableCell component="th" scope="row">{row.description}</TableCell>
+                    <TableCell>{row.mtime}</TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 48 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25]}
+                  colSpan={3}
+                  count={rows.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  onChangePage={this.handleChangePage}
+                  onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActionsWrapped}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+      </Paper>
+    );
+  }
+}
+
+ThreadDisplay.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ThreadDisplay);
